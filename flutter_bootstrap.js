f0035599a2fcc206bc14bed22bd83835e5003c01
CupertinoStudios <cupertinostudios@gmail.com>
2024-07-20 23:01:42 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "242392152"
+    serviceWorkerVersion: "1906965499"
   }
 });
